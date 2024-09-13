79d887cf31f3e2d2b63a9246eeff59b7118d805d
davidliu40313 <david.liu40313@gmail.com>
2024-09-13 01:35:25 +0000

    fix issue 3606 Co-authored-by: tapeimrchuang <sean88.chuang@gmail.com> Co-authored-by: markson0304 <markson0304@gmail.com> Co-authored-by: renenxie <eric4399@gmail.com>
    Signed-off-by: davidliu40313 <david.liu40313@gmail.com>

--- pkg/webhook/admission/pod/storage_initializer_injector.go
+++ pkg/webhook/admission/pod/storage_initializer_injector.go
@@ -351,7 +351,7 @@ func (mi *StorageInitializerInjector) InjectStorageInitializer(pod *v1.Pod) erro
 		if envVar.Name == constants.CustomSpecStorageMountPathKey && envVar.Value != "" {
 			sharedVolumeWriteMount.MountPath = envVar.Value
 		} else if envVar.Name == constants.CustomSpecStorageMountPathKey && envVar.Value == "" {
-			sharedVolumeWriteMount.MountPath = constants.DefaultModelLocalMountPath 
+			sharedVolumeWriteMount.MountPath = constants.DefaultModelLocalMountPath
 		}
 	}
 	storageInitializerMounts = append(storageInitializerMounts, sharedVolumeWriteMount)
@@ -368,7 +368,7 @@ func (mi *StorageInitializerInjector) InjectStorageInitializer(pod *v1.Pod) erro
 	for _, envVar := range userContainer.Env {
 		if envVar.Name == constants.CustomSpecStorageMountPathKey && envVar.Value != "" {
 			args = []string{srcURI, envVar.Value}
-			break 
+			break
 		}
 	}
 	// Add an init container to run provisioning logic to the PodSpec
